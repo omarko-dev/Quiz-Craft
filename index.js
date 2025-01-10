7ca15aa60e75353a0fd4b3dba926540e3eca20a3
omarko <omaralhamwi999888@gmail.com>
2025-01-10 21:17:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-document.addEventListener("DOMContentLoaded", () => {
-    new Game();
-  });
+document.getElementById('start-game-button').addEventListener('click', function() {
+  document.getElementById('main-menu').style.display = 'none';
+  document.getElementById('game-interface').style.display = 'block';
+  const backgroundMusic = document.getElementById('background-music');
+  backgroundMusic.volume = 0.4;
+  backgroundMusic.play();
+});
+
+const phrases = [
+  "Crafting Fun!",
+  "Quiz Time!",
+  "Are you ready?",
+  "Let's craft!",
+  "Test your skills!"
+];
+
+function displayRandomPhrase() {
+  const randomIndex = Math.floor(Math.random() * phrases.length);
+  const randomPhrase = phrases[randomIndex];
+  document.getElementById('random-phrase').textContent = randomPhrase;
+}
+
+displayRandomPhrase();
+
+let score = 0;
+
+function updateScore(points) {
+  score += points;
+  document.getElementById('score').textContent = score;
+}
+
+
+function checkAnswer(isCorrect) {
+  if (isCorrect) {
+    updateScore(10);
+  }
+}
+
+
+document.getElementById('submit-button').addEventListener('click', function() {
+  const isCorrect = true;
+  checkAnswer(isCorrect);
+});
+
+const volumeToggle = document.getElementById('volume-toggle');
+const volumeIcon = document.getElementById('volume-icon');
+const backgroundMusic = document.getElementById('background-music');
+
+volumeToggle.addEventListener('click', function() {
+  if (backgroundMusic.paused) {
+    backgroundMusic.play();
+    volumeIcon.src = './assets/volume.png';
+  } else {
+    backgroundMusic.pause();
+    volumeIcon.src = './assets/no-volume.png';
+  }
+});
